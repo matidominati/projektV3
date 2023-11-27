b33f7857fd75d8474eb170407d01406c340be21f
matidominati <mateuszdermont@gmail.com>
2023-11-28 00:52:29 +0100

    + API documentation

--- src/main/java/pl/matidominati/GitHubApp/controller/Controller.java
+++ src/main/java/pl/matidominati/GitHubApp/controller/Controller.java
@@ -1,6 +1,14 @@
 package pl.matidominati.GitHubApp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +25,29 @@ public class Controller {
 
     private final ServiceImpl service;
 
+    @Operation(summary = "Get repository details", tags = "GitHubApp")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = GitHubRepo.class))}),
+            @ApiResponse(responseCode = "404", description = "Data not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorResponse.class))})
+    })
     @GetMapping("/{owner}/{repo}")
     public GitHubRepo getRepositoryDetails(@PathVariable String owner, @PathVariable String repo) {
         return service.getRepositoryDetails(owner, repo);
     }
 
+    @Operation(summary = "Get user repositories", tags = "GitHubApp")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = GitHubRepo.class)))}),
+            @ApiResponse(responseCode = "404", description = "Data not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorResponse.class))})
+    })
     @GetMapping("/{owner}")
     public List<GitHubRepo> getRepositories(@PathVariable String owner) {
         return service.getRepository(owner);
