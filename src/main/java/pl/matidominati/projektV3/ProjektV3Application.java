d06bbcb153e3831bc31c983337a3f18bf8f82222
matidominati <mateuszdermont@gmail.com>
2023-11-27 20:31:15 +0100

    Initial commit

---
+++ src/main/java/pl/matidominati/projektV3/ProjektV3Application.java
@@ -0,0 +1,13 @@
+package pl.matidominati.projektV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/matidominati/projektV3/ProjektV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matidominati.projektV3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
