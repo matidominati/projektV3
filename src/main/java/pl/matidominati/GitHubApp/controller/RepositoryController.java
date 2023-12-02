894c4dde0dfc0940e9f01fe6d653fec196d4cf27
matidominati <mateuszdermont@gmail.com>
2023-12-02 02:06:48 +0100

    + api docs

--- src/main/java/pl/matidominati/GitHubApp/controller/RepositoryController.java
+++ src/main/java/pl/matidominati/GitHubApp/controller/RepositoryController.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import pl.matidominati.GitHubApp.client.model.GitHubRepository;
+import pl.matidominati.GitHubApp.model.dto.UpdateRepositoryResponseDto;
 import pl.matidominati.GitHubApp.model.pojo.RepositoryPojo;
 import pl.matidominati.GitHubApp.service.GitHubService;
 
@@ -23,7 +24,7 @@ public class RepositoryController {
 
     private final GitHubService gitHubService;
 
-    @Operation(summary = "Get repository details", tags = "GitHubApp")
+    @Operation(summary = "Get GitHub repository details", tags = "GitHubApp")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK",
                     content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
@@ -37,7 +38,7 @@ public class RepositoryController {
         return gitHubService.getClientRepositoryDetails(gitHubOwner, repoName);
     }
 
-    @Operation(summary = "Get user repositories", tags = "GitHubApp")
+    @Operation(summary = "Get GitHub user repositories", tags = "GitHubApp")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK",
                     content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
@@ -51,22 +52,55 @@ public class RepositoryController {
         return gitHubService.getClientRepositories(gitHubOwner);
     }
 
+    @Operation(summary = "Get local repository", tags = "GitHubApp")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = RepositoryResponseDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Data not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorResponse.class))})
+    })
     @GetMapping("/local/repositories/{owner}/{repoName}")
     public RepositoryResponseDto getLocalRepository(@PathVariable String owner, @PathVariable String repoName) {
         return gitHubService.getLocalRepositoryDetails(owner, repoName);
     }
 
+    @Operation(summary = "Save new repository", tags = "GitHubApp")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = UpdateRepositoryResponseDto.class))}),
+            @ApiResponse(responseCode = "409", description = "Data already exists",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorResponse.class))})
+    })
     @PostMapping("/repositories/{owner}/{repoName}")
     public RepositoryResponseDto saveRepository(@PathVariable String owner, @PathVariable String repoName) {
         return gitHubService.saveRepositoryDetails(owner, repoName);
     }
-
+    @Operation(summary = "Edit local repository", tags = "GitHubApp")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = RepositoryResponseDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Data not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorResponse.class))})
+    })
     @PutMapping("/repositories/{owner}/{repoName}")
     public RepositoryResponseDto editLocalRepository(@PathVariable String owner, @PathVariable String repoName,
                                                      @RequestBody RepositoryPojo updatedDetails) {
         return gitHubService.editLocalRepositoryDetails(owner, repoName, updatedDetails);
     }
 
+    @Operation(summary = "Delete local repository from DB", tags = "GitHubApp")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Data not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorResponse.class))})
+    })
     @DeleteMapping("/repositories/{owner}/{repoName}")
     public void deleteRepository(@PathVariable String owner, @PathVariable String repoName){
         gitHubService.deleteLocalRepositoryDetails(owner, repoName);
